15a76dccc7168374138694643bdf345ceb752ddf
jojolete <dyfraselle@gmail.com>
2023-07-21 16:34:37 +0200

    fuck me dead

--- core/auth/serializers/login.py
+++ core/auth/serializers/login.py
@@ -2,8 +2,8 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.settings import api_settings
 from django.contrib.auth.models import update_last_login
 from core.user.serializers import UserSerializer
-from core.abstract.serializers import AbstractSerializer
-class LoginSerializer(TokenObtainPairSerializer, AbstractSerializer):
+
+class LoginSerializer(TokenObtainPairSerializer):
     def validate(self, attrs): 
         data= super().validate(attrs)
 
